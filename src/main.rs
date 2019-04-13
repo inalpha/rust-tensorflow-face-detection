fe9578bee7e8ffc5bf380d4c24f843c24e1a205b
Mohamed Meftah <mohamed-ted@live.com>
2019-04-13 21:00:53 +0100

    load model and read args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error; 
+
+use tensorflow::Graph;
+use tensorflow::ImportGraphDefOptions;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Config {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    #[structopt(parse(from_os_str))]
+    output: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let model = include_bytes!("model.pb");
+    let config = Config::from_args();
+    let mut graph = Graph::new();
+    graph.import_graph_def(&*model, &ImportGraphDefOptions::new())?;
+
+    Ok(())
 }

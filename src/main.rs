fd5dc99f9319a9d7887e8e3ee0cdb82c05678a33
Mohamed Meftah <mohamed-ted@live.com>
2019-04-14 12:06:07 +0100

    returning bboxes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::sync::Arc;
+
+use log::info;
 use structopt::StructOpt;
-use log::debug;
+
+mod mtcnn;
+use mtcnn::{BBox, MTCNN};
+
+use actix_web::{middleware, web, App, Error as ActixError, HttpResponse, HttpServer};
+use futures::{Future, Stream};
+
+use image::DynamicImage;
 
 #[derive(StructOpt)]
 struct Config {
@@ -14,17 +24,47 @@ struct Config {
     listen: String,
 }
 
-
-
+type WebMTCNN = web::Data<Arc<MTCNN>>;
 
 fn main() -> Result<(), Box<dyn Error>> {
     if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "rust-tensorflow-face-detection=DEBUG,actix_web=DEBUG")
+        env::set_var("RUST_LOG", "mtcnn=DEBUG,actix_web=DEBUG")
     }
     pretty_env_logger::init_timed();
     let config = Config::from_args();
-    
-    debug!("{}", config.listen);
+    info!("Listening on: {}", config.listen);
 
-    Ok(())
+    let mtcnn = Arc::new(MTCNN::new()?);
+
+    Ok(HttpServer::new(move || {
+        App::new()
+            .data(mtcnn.clone())
+            .wrap(middleware::Logger::default())
+            .service(web::resource("api/v1/bboxes").to_async(handle_bboxes))
+    })
+    .bind(&config.listen)?
+    .run()?)
+}
+
+fn get_image(stream: web::Payload) -> impl Future<Item = DynamicImage, Error = ActixError> {
+    stream
+        .concat2()
+        .from_err()
+        .and_then(move |bytes| web::block(move || image::load_from_memory(&bytes)).from_err())
+}
+
+fn get_bboxes(
+    img: DynamicImage,
+    mtcnn: WebMTCNN,
+) -> impl Future<Item = Vec<BBox>, Error = ActixError> {
+    web::block(move || mtcnn.run(&img).map_err(|e| e.to_string())).from_err()
+}
+
+fn handle_bboxes(
+    stream: web::Payload,
+    mtcnn: WebMTCNN,
+) -> impl Future<Item = HttpResponse, Error = ActixError> {
+    get_image(stream)
+        .and_then(move |img| get_bboxes(img, mtcnn))
+        .map(|bboxes| HttpResponse::Ok().json(bboxes))
 }
--- src/mtcnn.rs
+++ src/mtcnn.rs
@@ -1,16 +1,20 @@
 use serde_derive::Serialize;
+use std::error::Error;
 
-use tensorflow::{Graph, ImportGraphDefOptions, Tensor};
+
+use tensorflow::{Graph, ImportGraphDefOptions, Tensor, Status};
 use tensorflow::{Session, SessionOptions, SessionRunArgs};
 
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, Rgba};
+
+use log::debug;
 
 pub struct MTCNN {
     graph: Graph,
     session: Session,
     min_size: Tensor<f32>,
     thresholds: Tensor<f32>,
-    factor: Tensor<f32>
+    factor: Tensor<f32>,
 }
 
 #[derive(Copy, Clone, Debug, Serialize)]
@@ -21,7 +25,6 @@ pub struct BBox {
     pub y2: f32,
     pub prob: f32,
 }
-
 
 const LINE_COLOUR: Rgba<u8> = Rgba {
     data: [0, 255, 0, 0],
@@ -35,90 +38,88 @@ impl MTCNN {
 
         let session = Session::new(&SessionOptions::new(), &graph)?;
 
-        let min_size = Tensor::new(&[]).with_values(&[40])?;
+        let min_size = Tensor::new(&[]).with_values(&[40.0])?;
         let thresholds = Tensor::new(&[3]).with_values(&[0.6, 0.7, 0.7])?;
         let factor = Tensor::new(&[]).with_values(&[0.709])?;
-        
+
         Ok(Self {
             graph,
             session,
             min_size,
             thresholds,
-            factor
+            factor,
         })
+    }
+    pub fn run(&self, img: &DynamicImage) -> Result<Vec<BBox>, Status> {
+        let input = {
+            let mut flattened = Vec::new();
 
-        pub fn run(&self, img: &DynamicImage) -> Result<Vec<BBox>, Status> {
-            let input = {
-                let mut flattened = Vec::new();
-
-                for (_x, _y, rgb) in img.pixels() {
-                    flattened.push(rgb[2] as f32);
-                    flattened.push(rgb[1] as f32);
-                    flattened.push(rgb[0] as f32);
-                }
-
-                Tensor::new(&[img.height() as u64, img.width() as u64, 3]).with_values(&flattened)?
+            for (_x, _y, rgb) in img.pixels() {
+                flattened.push(rgb[2] as f32);
+                flattened.push(rgb[1] as f32);
+                flattened.push(rgb[0] as f32);
             }
 
-            let mut args = SessionRunArgs::new();
-            args.add_feed(
-                &self.graph.operation_by_name_required("min_size")?,
-                0, 
-                &self.min_size,
-            );
-            args.add_feed(
-                &self.graph.operation_by_name_required("thresholds")?,
-                0,
-                &self.thresholds,
-            );
-            args.add_feed(
-                &self.graph.operation_by_name_required("factor")?,
-                0,
-                &self.factor
-            );
-            args.add_feed(
-                &self.graph.operation_by_name_required("input")?,
-                0,
-                input
-            );
+            Tensor::new(&[img.height() as u64, img.width() as u64, 3]).with_values(&flattened)?
+        };
 
-            let bbox = args.request_fetch(&self.graph.operation_by_name_required("box")?, 0);
-            let prob = args.request_fetch(&self.graph.operation_by_name_required("prob")?, 0);
+        let mut args = SessionRunArgs::new();
+        args.add_feed(
+            &self.graph.operation_by_name_required("min_size")?,
+            0,
+            &self.min_size,
+        );
+        args.add_feed(
+            &self.graph.operation_by_name_required("thresholds")?,
+            0,
+            &self.thresholds,
+        );
+        args.add_feed(
+            &self.graph.operation_by_name_required("factor")?,
+            0,
+            &self.factor,
+        );
+        args.add_feed(&self.graph.operation_by_name_required("input")?, 0, &input);
 
-            &self.session.run(&mut args)?;
+        let bbox = args.request_fetch(&self.graph.operation_by_name_required("box")?, 0);
+        let prob = args.request_fetch(&self.graph.operation_by_name_required("prob")?, 0);
 
-            let bbox_res: Tensor<f32> = args.fetch(bbox)?;
-            let prob_res: Tensor<f32> = args.fetch(prob)?;
+        &self.session.run(&mut args)?;
 
-            let mut bboxes = Vec::new();
+        let bbox_res: Tensor<f32> = args.fetch(bbox)?;
+        let prob_res: Tensor<f32> = args.fetch(prob)?;
 
-            let mut i = 0;
-            let mut j = 0;
+        let mut bboxes = Vec::new();
 
-            while i < bbox_res.len() {
-                bboxes.push(BBox {
-                    y1: bbox_res[i],
-                    x1: bbox_res[i + 1],
-                    y2: bbox_res[i + 2],
-                    x2: bbox_res[i + 3],
-                    prob: prob_res[j],
-                });
-                i += 4;
-                j += 1;
-            }
+        let mut i = 0;
+        let mut j = 0;
 
-            debug!("BBox Length: {}", bboxes.len());
-
-            Ok(bboxes)
+        while i < bbox_res.len() {
+            bboxes.push(BBox {
+                y1: bbox_res[i],
+                x1: bbox_res[i + 1],
+                y2: bbox_res[i + 2],
+                x2: bbox_res[i + 3],
+                prob: prob_res[j],
+            });
+            i += 4;
+            j += 1;
         }
-    }
 
-    pub fn overlay(img: &DynamicImage, bboxs: &Vec(BBox)) -> DynamicImage {
-        let mut output = img.clone();
-        for bbox in bboxs {
-            let rect = Rect::at(bbox.x1 as i32, bbox.y1 as i32)
-            draw_hollow_rect_mut(&mut output, rect, LINE_COLOUR)
-        }
-        output
+        debug!("BBox Length: {}", bboxes.len());
+
+        Ok(bboxes)
     }
+}
+pub fn overlay(img: &DynamicImage, bboxs: &Vec<BBox>) -> DynamicImage {
+    use imageproc::drawing::draw_hollow_rect_mut;
+    use imageproc::rect::Rect;
+
+    let mut output = img.clone();
+    for bbox in bboxs {
+        let rect = Rect::at(bbox.x1 as i32, bbox.y1 as i32)
+            .of_size((bbox.x2 - bbox.x1) as u32, (bbox.y2 - bbox.y1) as u32);;
+        draw_hollow_rect_mut(&mut output, rect, LINE_COLOUR);
+    }
+    output
 }

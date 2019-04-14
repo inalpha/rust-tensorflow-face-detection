a74e414e96822def6b4a17ae2ecd17afe5869982
Mohamed Meftah <mohamed-ted@live.com>
2019-04-14 12:54:07 +0100

    make a server

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use structopt::StructOpt;
 mod mtcnn;
 use mtcnn::{BBox, MTCNN};
 
+use actix_web::http::header::CONTENT_TYPE;
 use actix_web::{middleware, web, App, Error as ActixError, HttpResponse, HttpServer};
+use actix_web::http::{StatusCode, HeaderValue};
 use futures::{Future, Stream};
 
 use image::DynamicImage;
@@ -41,6 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .data(mtcnn.clone())
             .wrap(middleware::Logger::default())
             .service(web::resource("api/v1/bboxes").to_async(handle_bboxes))
+            .service(web::resource("api/v1/overlay").to_async(handle_overlay))
     })
     .bind(&config.listen)?
     .run()?)
@@ -56,8 +59,14 @@ fn get_image(stream: web::Payload) -> impl Future<Item = DynamicImage, Error = A
 fn get_bboxes(
     img: DynamicImage,
     mtcnn: WebMTCNN,
-) -> impl Future<Item = Vec<BBox>, Error = ActixError> {
-    web::block(move || mtcnn.run(&img).map_err(|e| e.to_string())).from_err()
+) -> impl Future<Item = (DynamicImage, Vec<BBox>), Error = ActixError> {
+    web::block(move || {
+        mtcnn
+            .run(&img)
+            .map_err(|e| e.to_string())
+            .map(|bboxes| (img, bboxes))
+    })
+    .from_err()
 }
 
 fn handle_bboxes(
@@ -66,5 +75,32 @@ fn handle_bboxes(
 ) -> impl Future<Item = HttpResponse, Error = ActixError> {
     get_image(stream)
         .and_then(move |img| get_bboxes(img, mtcnn))
-        .map(|bboxes| HttpResponse::Ok().json(bboxes))
+        .map(|(_img, bboxes)| HttpResponse::Ok().json(bboxes))
+}
+
+fn get_overlay(
+    img: DynamicImage,
+    bboxes: Vec<BBox>,
+) -> impl Future<Item = Vec<u8>, Error = ActixError> {
+    web::block(move || {
+        let output = mtcnn::overlay(&img, &bboxes);
+        let mut buffer = Vec::new();
+        output.write_to(&mut buffer, image::JPEG)?;
+        Ok(buffer) as Result<_, image::ImageError>
+    })
+    .from_err()
+}
+
+fn handle_overlay(
+    stream: web::Payload,
+    mtcnn: WebMTCNN,
+) -> impl Future<Item = HttpResponse, Error = ActixError> {
+    get_image(stream)
+        .and_then(move |img| get_bboxes(img, mtcnn))
+        .and_then(|(img, bboxes)| get_overlay(img, bboxes))
+        .map(|buffer| {
+            let mut response = HttpResponse::with_body(StatusCode::OK, buffer.into());
+            response.headers_mut().insert(CONTENT_TYPE, HeaderValue::from_static("image/jpeg"));
+            response
+        })
 }
